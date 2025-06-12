6676b6ff4ea36bf5eca0108eeec801e8dd6165c2
demisun12 <115199260+demisun12@users.noreply.github.com>
2025-06-11 20:38:30 -0700

    Wrote code for gameflow object

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const board = {
     row2:["","",""],
     row3:["","",""],
 };
+
+const playerOne = {
+    name: "",
+    marker: "X"
+};
+
+const playerTwo = {
+    name: "",
+    marker: "O"
+};  
+
+const gameFlow = {
+    turn: playerOne.name,
+    mark: playerOne.marker
+}; //after each turn, object values should change to other players name/markers
